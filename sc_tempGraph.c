2c5dcdfa0bbfbb5cd09859b9d14203764e4d93f4
pebble042 <ploy.yadfon@gmail.com>
2021-07-31 10:24:16 +0900

    edit sc all func switch page

--- sc_tempGraph.c
+++ sc_tempGraph.c
@@ -1,28 +1,10 @@
 #include "mng_window.h"
 
-static void to_home_page (LiteButton *button, void *ctx)
+static void change_page (LiteButton *button, void *ctx)
 {
-	
-	changeToPage  	(SC1_HOME);
-}
-
-static void to_graph_page(LiteButton *button,void *ctx)
-{
-	changeToPage  	(SC2_GRAPH);
-
-}
-
-
-static void to_calculator_page (LiteButton *button, void *ctx)
-{
-	
-	changeToPage  	(SC3_CALCULATOR);
-}
-
-static void to_other_page (LiteButton *button, void *ctx)
-{
-	
-	changeToPage  	(SC5_OTHER);
+	int id_page = (int)(long)ctx;
+	changeToPage  	(id_page);
+			
 }
 
 int sc_tempGraph(LiteWindow *window)
@@ -45,22 +27,22 @@ int sc_tempGraph(LiteWindow *window)
 	rect.x = 0;	rect.y = 200;	rect.w = 200;	rect.h = 45;
 	lite_new_button 				( box , &rect, liteDefaultButtonTheme, &btnHome_tmpGraph);
 	lite_set_button_image  			( btnHome_tmpGraph,	LITE_BS_PRESSED, 	"button/btn_home_press.png");
-	lite_on_button_press  			( btnHome_tmpGraph,	to_home_page, 	(void *)(long)1);
+	lite_on_button_press  			( btnHome_tmpGraph,	change_page, 	(void *)(long)SC1_HOME);
 
 	rect.x = 0;	rect.y = 245;	rect.w = 200;	rect.h = 45;
 	lite_new_button 				( box , &rect, liteDefaultButtonTheme, &btnGraph_tmpGraph);
 	lite_set_button_image  			( btnGraph_tmpGraph,	LITE_BS_PRESSED, 	"button/btn_graph_press.png");
-	lite_on_button_press  			( btnGraph_tmpGraph,	to_graph_page, 	(void *)(long)1);
+	lite_on_button_press  			( btnGraph_tmpGraph,	change_page, 	(void *)(long)SC2_GRAPH);
 	
 	rect.x = 0;	rect.y = 290;	rect.w = 200;	rect.h = 45;
 	lite_new_button 				( box , &rect, liteDefaultButtonTheme, &btnCalculator_tmpGraph);
 	lite_set_button_image  			( btnCalculator_tmpGraph,	LITE_BS_PRESSED, 	"button/btn_cal_press.png");
-	lite_on_button_press  			( btnCalculator_tmpGraph,	to_calculator_page, 	(void *)(long)1);
+	lite_on_button_press  			( btnCalculator_tmpGraph,	change_page, 	(void *)(long)SC3_CALCULATOR);
 
 	rect.x = 0;	rect.y = 380;	rect.w = 200;	rect.h = 45;
 	lite_new_button 				( box , &rect, liteDefaultButtonTheme, &btnOther_tmpGraph);
 	lite_set_button_image  			( btnOther_tmpGraph,	LITE_BS_PRESSED, 	"button/btn_other_press.png");
-	lite_on_button_press  			( btnOther_tmpGraph,	to_other_page, 	(void *)(long)1);
+	lite_on_button_press  			( btnOther_tmpGraph,	change_page, 	(void *)(long)SC5_OTHER);
 
 
 
